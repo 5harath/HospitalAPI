54c895bbc1d999fcbd994717b19e8a7bd07feb1e
5harath <19461590+5harath@users.noreply.github.com>
2023-04-30 17:26:46 +0530

    change name

--- router/patient.js
+++ router/patient.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../config/auth');
 const patient = require('../controller/patient');
-router.post('/registers',patient.register);
+router.post('/register',patient.register);
 module.exports = router;
